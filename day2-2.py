4cb6bde4ce21f5b66c586cf0ed674b88e6ae15fa
swalih33 <137264504+swalih33@users.noreply.github.com>
2023-07-05 23:40:59 +0530

    Add files via upload

---
+++ day2-2.py
@@ -0,0 +1,9 @@
+a= input("Enter a foat value : ")
+b= input("Enter a integer value : ")
+# print("you entered input is : ", user_intput a)
+# print("you entered input is : ", user_intput b)
+
+sum=(a + b)
+print("the sum is:",sum)
+
+
