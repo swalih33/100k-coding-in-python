e16217db5493fd61e8ce7cdd1368eea9a592a0a0
swalih33 <137264504+swalih33@users.noreply.github.com>
2023-07-06 12:41:17 +0530

    Add files via upload

---
+++ day-3-3.py
@@ -0,0 +1,22 @@
+ 
+num=int(input("enter a value between 1 to 7: "))
+print("you enterd",num)
+if(num==1):
+    print("monday")
+elif(num==2):
+    print("tuesday")
+elif(num==3):
+    print("wednesday")
+elif(num==4):
+    print("thursday")
+elif(num==5):
+    print("friday")
+elif(num==6):
+    print("saturday")
+elif(num==7):
+    print("sunday")
+else:
+    print("invalid entry")
+ 
+  
+
