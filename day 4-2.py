dbd1f29257e48fdc55ff59404e26ced9e49dcef6
swalih33 <137264504+swalih33@users.noreply.github.com>
2023-07-06 22:19:55 +0530

    Add files via upload

---
+++ day 4-2.py
@@ -0,0 +1,18 @@
+num1=int(input("enter a limit :"))
+i=1
+sum=0
+while(i<=num1):
+    if(i%2==1):
+        print(i)
+        sum=sum+i
+        
+    i=i+1
+    # print(i)
+    
+print("sum",sum)   
+    
+
+
+
+
+
